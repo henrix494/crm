c133f16d7264c1b2b91b93bfd72273d66b66feef
henrix49 <henrix494@gmail.com>
2023-07-04 22:42:53 +0300

    first commit

--- server/utils/mongo.js
+++ server/utils/mongo.js
@@ -4,7 +4,7 @@ const mongoose = require(`mongoose`);
 async function connectToDB() {
 	mongoose.set("strictQuery", true);
 	try {
-		await mongoose.connect({
+		await mongoose.connect(process.env.MONGO_URL, {
 			dbName: "crm",
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
