84a6538ceaa75cb88fce50218f02711465be8614
henrix49 <henrix494@gmail.com>
2023-07-05 10:27:44 +0300

    footer added

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -39,8 +39,25 @@ export default function Contact() {
 		postData();
 	};
 	return (
-		<div className="  relative h-[600px] max-md:flex-col items-center  flex justify-around bg-[url('./assets/fotter.png')] bg-cover bg-center  after:contents-[``] after:absolute after:bg-black after:w-[100%] after:h-[100%] after:opacity-60 max-md:after:opacity-60 after:z-40 z-30">
-			<div className=" border-2 border-[white] flex-[0.7] z-[500] "></div>
+		<div className="  relative h-[600px] max-md:h-full max-md:flex-col  flex justify-around bg-[url('./assets/fotter.png')] bg-cover bg-center  after:contents-[``] after:absolute after:bg-black after:w-[100%] after:h-[100%] after:opacity-60 max-md:after:opacity-60 after:z-40 z-30">
+			<div className=" mt-[6%] gap-5  text-white   text-center flex flex-col items-center pt-5  flex-[0.7] z-[500] ">
+				<div>
+					<h3 className="pt-5  text-4xl max-md:text-2xl">קנו עכשיו</h3>
+				</div>
+				<div>
+					{" "}
+					<h2 className="text-2xl">הנחה חד פעמית של כ 20% </h2>
+				</div>
+				<div className=" text-3xl">
+					<h2>רק ב 1499 במקום ב 2000 שקלים</h2>
+				</div>
+				<div className="button-container-2 flex m-auto  mt-[8%] mb-[2%]">
+					<span className="mas"> קנה עכשיו</span>
+					<button type="submit" name="Hover">
+						קנה עכשיו{" "}
+					</button>
+				</div>
+			</div>
 			<div className="z-[500] text-white flex-1  text-center flex flex-col items-center justify-center">
 				<h4 className="pt-5  text-4xl max-md:text-2xl">
 					אל תפספסו את המכונה ששיגעה את אסיה
