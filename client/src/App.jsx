c654943ebca644a09b897461c9dc58ab59f72721
henrix49 <henrix494@gmail.com>
2023-07-09 15:43:01 +0300

    footer fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,11 @@ import Contact from "./components/contact/Contact";
 import { Route, Routes } from "react-router-dom";
 import Checkout from "./components/checkout/Checkout";
 import Navbar from "./components/navbar/Navbar";
+import ReactGA from "react-ga";
+
 function App() {
+	const TRACKING_ID = "G-HSZC51BVVN"; // YOUR_OWN_TRACKING_ID
+	ReactGA.initialize(TRACKING_ID);
 	return (
 		<>
 			<div className=" fixed top-0 w-full  h-[35px] bg-[#655032] z-[5000000]">
