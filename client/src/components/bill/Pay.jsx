e625f78e101fed8c06b7cbc78aa5ad9396d6d93d
henrix49 <henrix494@gmail.com>
2023-07-30 14:35:28 +0300

    fixed some styles

--- client/src/components/bill/Pay.jsx
+++ client/src/components/bill/Pay.jsx
@@ -68,7 +68,7 @@ export default function Pay() {
 						}
 					);
 					const final = await zCredit.json();
-					console.log(final);
+					console.log(final.sessionUrl.Data);
 					// window.open(final.Data.SessionUrl);
 				}
 				if (typeOfPay === "paypal") {
